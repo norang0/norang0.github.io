cae0e201eb8e605d2964fdfac6b16b3ff6f53988
norang0 <kyuheehong0@gmail.com>
2023-03-28 23:04:03 +0900

    commit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "github.io 블로그 시작하기",
+        "excerpt":"Hello, blog   이 글의 제목은 github.io 블로그 시작하기이고 마지막으로 수정된 시간은 2023-03-28 08:06:00 -0500이다.  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "/blog/fist-post/",
+        "teaser": null
+      }]
