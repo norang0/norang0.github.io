a9c0f93197706bf384744f90ff23bd6dfc36c0b3
norang0 <kyuheehong0@gmail.com>
2023-03-29 22:28:30 +0900

    20230329 blog post, add profile photo

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["Blog"],
         "url": "/blog/fist-post/",
         "teaser": null
+      },{
+        "title": "Swift 공부에 앞서",
+        "excerpt":"Swift 공부를 하기로 했다.  각종 인터넷 강좌들을 찾아보다가 종이책도 있으면 좋을 것 같아서 주문했다.      가장 인기가 많은 책으로 주문했는데 (꼼꼼한 재은씨 실전편은 품절이라 못 구했다)  생각보다 볼륨이 엄청나고 진짜 무겁다..!  열심히 보고 재밌게 공부해야지.  ","categories": ["Blog"],
+        "tags": ["Blog, Swift"],
+        "url": "/blog/new-books/",
+        "teaser": null
       }]
